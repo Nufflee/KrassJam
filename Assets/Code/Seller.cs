8e32712682e4edf5315cfa6e8b33d1d4abd52dcc
Nufflee <zifcani@gmail.com>
2017-11-18 14:28:52 +0100

    Initial commit.

---
+++ Assets/Code/Seller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Seller : MonoBehaviour
+{
+  // Use this for initialization
+  public void Start()
+  {
+    GameObject sellerItem = Resources.Load<GameObject>("Prefabs/SellerItem");
+
+    //343.96
+    Instantiate(sellerItem, transform, false).GetComponent<RectTransform>().sizeDelta = new Vector2(349.1f, 113.6f);
+    Instantiate(sellerItem, transform, false).GetComponent<RectTransform>().sizeDelta = new Vector2(349.1f, 113.6f);
+    Instantiate(sellerItem, transform, false).GetComponent<RectTransform>().sizeDelta = new Vector2(349.1f, 113.6f);
+    Instantiate(sellerItem, transform, false).GetComponent<RectTransform>().sizeDelta = new Vector2(349.1f, 113.6f);
+  }
+
+  // Update is called once per frame
+  public void Update()
+  {
+  }
+}
